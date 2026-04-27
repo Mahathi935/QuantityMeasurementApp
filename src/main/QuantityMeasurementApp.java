47f3aca1dff069f04dbd00b7004f842badbebde8
Moonlily <mahathit.593@gmail.com>
2026-04-27 23:22:51 +0530

    UC2: Feet and Inches equality with unit conversion

--- src/main/QuantityMeasurementApp.java
+++ src/main/QuantityMeasurementApp.java
@@ -1,9 +1,12 @@
 /**
  * QuantityMeasurementApp
  *
- * UC2: Feet and Inches Equality
+ * UC2: Feet and Inches Equality (with cross-unit comparison)
  *
- * Demonstrates equality comparison for both Feet and Inches.
+ * Supports:
+ * 1. Feet vs Feet
+ * 2. Inches vs Inches
+ * 3. Feet vs Inches (conversion)
  *
  * @author Mahathi
  * @version 1.0
@@ -19,13 +22,26 @@ public class QuantityMeasurementApp {
             this.value = value;
         }
 
+        public double toInches() {
+            return value * 12;
+        }
+
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
-            if (obj == null || this.getClass() != obj.getClass()) return false;
+            if (obj == null) return false;
 
-            Feet other = (Feet) obj;
-            return Double.compare(this.value, other.value) == 0;
+            if (obj instanceof Feet) {
+                Feet other = (Feet) obj;
+                return Double.compare(this.value, other.value) == 0;
+            }
+
+            if (obj instanceof Inches) {
+                Inches other = (Inches) obj;
+                return Double.compare(this.toInches(), other.value) == 0;
+            }
+
+            return false;
         }
     }
 
@@ -37,33 +53,48 @@ public class QuantityMeasurementApp {
             this.value = value;
         }
 
+        public double toFeet() {
+            return value / 12;
+        }
+
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
-            if (obj == null || this.getClass() != obj.getClass()) return false;
+            if (obj == null) return false;
 
-            Inches other = (Inches) obj;
-            return Double.compare(this.value, other.value) == 0;
+            if (obj instanceof Inches) {
+                Inches other = (Inches) obj;
+                return Double.compare(this.value, other.value) == 0;
+            }
+
+            if (obj instanceof Feet) {
+                Feet other = (Feet) obj;
+                return Double.compare(this.value, other.toInches()) == 0;
+            }
+
+            return false;
         }
     }
 
     // -------- STATIC METHODS --------
     public static boolean compareFeet(double v1, double v2) {
-        Feet f1 = new Feet(v1);
-        Feet f2 = new Feet(v2);
-        return f1.equals(f2);
+        return new Feet(v1).equals(new Feet(v2));
     }
 
     public static boolean compareInches(double v1, double v2) {
-        Inches i1 = new Inches(v1);
-        Inches i2 = new Inches(v2);
-        return i1.equals(i2);
+        return new Inches(v1).equals(new Inches(v2));
+    }
+
+    public static boolean compareFeetAndInches(double feet, double inches) {
+        return new Feet(feet).equals(new Inches(inches));
     }
 
     // -------- MAIN --------
     public static void main(String[] args) {
 
-        System.out.println("Feet Comparison: " + compareFeet(1.0, 1.0));
-        System.out.println("Inches Comparison: " + compareInches(1.0, 1.0));
+        System.out.println("Feet vs Feet: " + compareFeet(1.0, 1.0));
+        System.out.println("Inches vs Inches: " + compareInches(1.0, 1.0));
+        System.out.println("Feet vs Inches (1 ft vs 12 in): " +
+                compareFeetAndInches(1.0, 12.0));
     }
 }
